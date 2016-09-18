a59af0032227e08e6948ee3fa16d0c668bbe8886
webDeveloper-R <webdeveloper0423@gmail.com>
2016-09-18 19:59:00 +0300

    commit-02-added reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,7 +8,51 @@
  */
 /* Populated by react-webpack-redux:reducer */
 import { combineReducers } from 'redux';
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from '../actions/const'
+const { SHOW_ALL } = VisibilityFilters;
 
+/*
 const reducers = {};
 const combined = combineReducers(reducers);
 module.exports = combined;
+*/
+
+function visibilityFilter(state = SHOW_ALL, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter
+    default:
+      return state
+  }
+}
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+      {
+        text: action.text,
+        completed: false
+      }
+    ]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+          if (index === action.index) {
+      return Object.assign({}, todo, {
+        completed: !todo.completed
+      })
+    }
+      return todo
+  })
+  default:
+    return state
+  }
+}
+
+const todoApp = combineReducers({
+  visibilityFilter,
+  todos
+})
+
+export default todoApp
